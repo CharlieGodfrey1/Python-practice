7cc8788fcb7e11a51f46076d2f9efab5831ba18e
Luke Smyth <smythluke123@gmail.com>
2020-02-20 19:16:19 +0000

    rewrite

--- Practice Project.py
+++ Practice Project.py
@@ -1,16 +1,11 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+# part 1
+for i in range(1, 101):
+    if i**2<200:
+        print("i: " + str(i) + ". i^2: " + str(i**2))
 
-print(list)
+
+# part 2     
 user_number=int(input("please enter a number from 1-100: "))
-for i in list:
-    if user_number>=i:
-        print("Regular numbers")
-        print(i)
-        print("Square number")
-        print(i**2)
-
-        
-
-
-
-
+for i in range(1, 101):
+    if i**2<user_number:
+        print("i: " + str(i) + ". i^2: " + str(i**2))
